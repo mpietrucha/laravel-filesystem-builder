c0b3a1d00648fc85dcc21d491f490295205fdb70
Michał Pietrucha <michal.pietrucha@off-site.pl>
2023-04-06 10:15:43 +0200

    wip

---
+++ src/Builder.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Mpietrucha\Laravel\Filesystem;
+
+use Exception;
+use Symfony\Component\Finder\SplFileInfo;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Arr;
+use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Support\Collection;
+use Mpietrucha\Support\Concerns\HasFactory;
+use Mpietrucha\Support\Types;
+
+class Builder
+{
+    use HasFactory;
+
+    protected ?string $root = null;
+
+    public function __construct(protected Filesystem|FilesystemAdapter $adapter)
+    {
+    }
+
+    public function files(?string $path): Collection
+    {
+        return collect($this->adapter->files($path))->map($this->build(...));
+    }
+
+    public function allFiles(?string $path): Collection
+    {
+        $this->assertPath($path);
+
+        return collect($this->adapter->allFiles($path))->map($this->build(...));
+    }
+
+    protected function build(string|SplFileInfo $file): SplFileInfo
+    {
+        if (! Types::string($file)) {
+            return $file;
+        }
+
+        return new SplFileInfo(
+            collect([$this->root(), $file])->toDirectory()
+        );
+    }
+
+    protected function root(): ?string
+    {
+        return $this->root ??= Arr::get($this->adapter->config, 'root', '');
+    }
+
+    protected function assertPath(?string $path): void
+    {
+        if (! $this->adapter instanceOf Filesystem) {
+            return;
+        }
+
+        if (! Types::null($path)) {
+            return;
+        }
+
+        throw new Exception('Path cannot be empty in Filesystem.');
+    }
+}
